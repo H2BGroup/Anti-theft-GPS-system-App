21d6f584fa3b16f5c5bec67e2a6f6ba5a13a5bef
Hunrax <radoslawgajewski03@gmail.com>
2024-08-27 20:09:57 +0200

    fixed a bug with wrong page display after clicking to edit textfields

--- atgs_app/lib/pages/device.dart
+++ atgs_app/lib/pages/device.dart
@@ -40,80 +40,83 @@ IconData returnBatteryStatusIcon() {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Align(
-          alignment: Alignment.center,
-          child: Container(
-            margin: const EdgeInsets.only(top: 50),
-            constraints: const BoxConstraints(maxHeight: 570),
-            width: 330,
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(color: darkBlue, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 5)),
-            child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            
-            children: [
-              Text("Device status", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 32, fontWeight: FontWeight.w900)),
-              const SizedBox(height: 45),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 90),
+        child: Center(
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              constraints: const BoxConstraints(maxHeight: 570),
+              width: 330,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(color: darkBlue, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 5)),
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              
+              children: [
+                Text("Device status", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 32, fontWeight: FontWeight.w900)),
+                const SizedBox(height: 45),
 
-              Container(
-                padding: const EdgeInsets.all(10.0),
-                width: 330,
-                decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
-                child: Column(
-                  children: [
-                    Text( (deviceArmed) ? "Device: Armed" : "Device: Disarmed", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
-                    SizedBox(
-                      width: 80,
-                      height: 60,
-                      child: FittedBox(
-                        fit: BoxFit.fill,
-                        child: 
-                        Row(children: [
-                          Switch(
-                            value: deviceArmed,
-                            activeColor: darkestBlue,
-                            activeTrackColor: darkBlue,
-                            inactiveTrackColor: lightBlue,
-                            inactiveThumbColor: darkBlue,
-                            trackOutlineColor: deviceArmed ? WidgetStateProperty.all(darkestBlue) : WidgetStateProperty.all(darkBlue),
-                            onChanged: (bool value) {
-                              setState(() { deviceArmed = value;});
-                            }
-                          )
-                        ])
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  width: 330,
+                  decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
+                  child: Column(
+                    children: [
+                      Text( (deviceArmed) ? "Device: Armed" : "Device: Disarmed", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
+                      SizedBox(
+                        width: 80,
+                        height: 60,
+                        child: FittedBox(
+                          fit: BoxFit.fill,
+                          child: 
+                          Row(children: [
+                            Switch(
+                              value: deviceArmed,
+                              activeColor: darkestBlue,
+                              activeTrackColor: darkBlue,
+                              inactiveTrackColor: lightBlue,
+                              inactiveThumbColor: darkBlue,
+                              trackOutlineColor: deviceArmed ? WidgetStateProperty.all(darkestBlue) : WidgetStateProperty.all(darkBlue),
+                              onChanged: (bool value) {
+                                setState(() { deviceArmed = value;});
+                              }
+                            )
+                          ])
+                        )
                       )
-                    )
-                  ],
-                )
-              ),
-              const SizedBox(height: 45),
+                    ],
+                  )
+                ),
+                const SizedBox(height: 45),
 
-              Container(
-                padding: const EdgeInsets.all(15.0),
-                width: 330,
-                decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
-                child: Column(
-                  children: [
-                    Text("Battery: $batteryStatus%", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
-                    Badge(isLabelVisible: false, child: Icon( returnBatteryStatusIcon(), color: darkestBlue, size: 35)),
-                  ]
-                )
-              ),         
-              const SizedBox(height: 45),
+                Container(
+                  padding: const EdgeInsets.all(15.0),
+                  width: 330,
+                  decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
+                  child: Column(
+                    children: [
+                      Text("Battery: $batteryStatus%", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
+                      Badge(isLabelVisible: false, child: Icon( returnBatteryStatusIcon(), color: darkestBlue, size: 35)),
+                    ]
+                  )
+                ),         
+                const SizedBox(height: 45),
 
-              Container(
-                padding: const EdgeInsets.all(15.0),
-                width: 330,
-                decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
-                child: Column(
-                  children: [
-                    Text( (signalConnection) ? "Signal: Available" : "Signal: Lost connection", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
-                    Badge(isLabelVisible: false, child: Icon( signalConnection ? Icons.signal_cellular_alt_rounded : Icons.signal_cellular_off_rounded, color: darkestBlue, size: 35)),                  
-                  ]
-                )
-              )   
-            ]
+                Container(
+                  padding: const EdgeInsets.all(15.0),
+                  width: 330,
+                  decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
+                  child: Column(
+                    children: [
+                      Text( (signalConnection) ? "Signal: Available" : "Signal: Lost connection", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
+                      Badge(isLabelVisible: false, child: Icon( signalConnection ? Icons.signal_cellular_alt_rounded : Icons.signal_cellular_off_rounded, color: darkestBlue, size: 35)),                  
+                    ]
+                  )
+                )   
+              ]
+              )
             )
           )
         )
--- atgs_app/lib/pages/profile.dart
+++ atgs_app/lib/pages/profile.dart
@@ -101,133 +101,136 @@ class ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child:  Align(
-          alignment: Alignment.center,
-          child: Container(
-            margin: const EdgeInsets.only(top: 50),
-            constraints: const BoxConstraints(maxHeight: 570),
-            width: 330,
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(color: darkBlue, borderRadius: BorderRadius.circular(25), border: Border.all(color: darkestBlue, width: 5)),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text("Profile settings", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 32, fontWeight: FontWeight.w900)),
-                const SizedBox(height: 20),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 90),
+        child: Center(
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              constraints: const BoxConstraints(maxHeight: 570),
+              width: 330,
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(color: darkBlue, borderRadius: BorderRadius.circular(25), border: Border.all(color: darkestBlue, width: 5)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Profile settings", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 32, fontWeight: FontWeight.w900)),
+                  const SizedBox(height: 20),
 
-                Container(
-                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                  width: 330,
-                  decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
-                  child: Column(
-                    children: [
-                      Text( "Device phone number", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
-                      const SizedBox(height: 5),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 240,
-                            height: 80,
-                            child: 
-                            TextField(
-                              controller: deviceNumberController,
-                              style: TextStyle(fontWeight: FontWeight.w700, foreground: Paint() ..color = Colors.white, fontSize: 20),
-                              cursorColor: darkestBlue,
-                              textAlign: TextAlign.center,
-                              keyboardType: TextInputType.number,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
-                              ],
-                              decoration: const InputDecoration(
-                                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
-                                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
-                                counterText: ''
-                              ),
-                              maxLength: 14,
-                            )
-                          )
-                        ]
-                      )
-                    ]
-                  )
-                ),
-                const SizedBox(height: 30),
-
-                Container(
-                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
-                  width: 330,
-                  decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
-                  child: Column(
-                    children: [
-                      Text( "Owner's phone number", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
-                      const SizedBox(height: 5),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          SizedBox(
-                            width: 240,
-                            height: 80,
-                            child: 
-                            TextField(
-                              controller: ownersNumberController,
-                              style: TextStyle(fontWeight: FontWeight.w700, foreground: Paint() ..color = Colors.white, fontSize: 20),
-                              cursorColor: darkestBlue,
-                              textAlign: TextAlign.center,
-                              keyboardType: TextInputType.number,
-                              inputFormatters: <TextInputFormatter>[
-                                FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
-                              ],
-                              decoration: const InputDecoration(
-                                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
-                                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
-                                counterText: ''
-                              ),
-                              maxLength: 14,
-                            )
-                          )
-                        ]
-                      )
-                    ]
-                  )
-                ),
-                const SizedBox(height: 30),
-
-                Container(
-                  padding: const EdgeInsets.all(10.0),
-                  width: 330,
-                  decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
-                  child: Column(
-                    children: [
-                      Text( "Subscription expires on", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
-                      const SizedBox(height: 5),
-                      SizedBox(
-                        width: 250,
-                        height: 75,
-                        child: 
-                          TextField(
-                              style: TextStyle(fontWeight: FontWeight.w700, foreground: Paint() ..color = Colors.white, fontSize: 20),
-                              cursorColor: darkestBlue,
-                              textAlign: TextAlign.center,
-                              controller: datePickerController,
-                              readOnly: true,
-                              decoration: InputDecoration(
-                                enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
-                                focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),                 
-                                hintText: "Click here to select date",
-                                hintStyle: TextStyle(fontWeight: FontWeight.w500, foreground: Paint() ..color = Colors.white, fontSize: 20, fontStyle: FontStyle.italic)
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                    width: 330,
+                    decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
+                    child: Column(
+                      children: [
+                        Text( "Device phone number", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
+                        const SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 240,
+                              height: 80,
+                              child: 
+                              TextField(
+                                controller: deviceNumberController,
+                                style: TextStyle(fontWeight: FontWeight.w700, foreground: Paint() ..color = Colors.white, fontSize: 20),
+                                cursorColor: darkestBlue,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+                                ],
+                                decoration: const InputDecoration(
+                                  enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
+                                  counterText: ''
                                 ),
-                              onTap: () => onTapDatePicker(context: context),
+                                maxLength: 14,
+                              )
                             )
-                      )
-                    ]
+                          ]
+                        )
+                      ]
+                    )
+                  ),
+                  const SizedBox(height: 30),
+
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                    width: 330,
+                    decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
+                    child: Column(
+                      children: [
+                        Text( "Owner's phone number", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
+                        const SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: 240,
+                              height: 80,
+                              child: 
+                              TextField(
+                                controller: ownersNumberController,
+                                style: TextStyle(fontWeight: FontWeight.w700, foreground: Paint() ..color = Colors.white, fontSize: 20),
+                                cursorColor: darkestBlue,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: <TextInputFormatter>[
+                                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+                                ],
+                                decoration: const InputDecoration(
+                                  enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
+                                  counterText: ''
+                                ),
+                                maxLength: 14,
+                              )
+                            )
+                          ]
+                        )
+                      ]
+                    )
+                  ),
+                  const SizedBox(height: 30),
+
+                  Container(
+                    padding: const EdgeInsets.all(10.0),
+                    width: 330,
+                    decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(25),border: Border.all(color: darkestBlue, width: 3)),
+                    child: Column(
+                      children: [
+                        Text( "Subscription expires on", style: TextStyle(foreground: Paint() ..color = Colors.white, fontSize: 24, fontWeight: FontWeight.w700)),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          width: 250,
+                          height: 75,
+                          child: 
+                            TextField(
+                                style: TextStyle(fontWeight: FontWeight.w700, foreground: Paint() ..color = Colors.white, fontSize: 20),
+                                cursorColor: darkestBlue,
+                                textAlign: TextAlign.center,
+                                controller: datePickerController,
+                                readOnly: true,
+                                decoration: InputDecoration(
+                                  enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),
+                                  focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: darkestBlue, width: 2.0)),                 
+                                  hintText: "Click here to select date",
+                                  hintStyle: TextStyle(fontWeight: FontWeight.w500, foreground: Paint() ..color = Colors.white, fontSize: 20, fontStyle: FontStyle.italic)
+                                  ),
+                                onTap: () => onTapDatePicker(context: context),
+                              )
+                        )
+                      ]
+                    )
                   )
-                )
-              ]
+                ]
+              )
             )
           )
-        )
+      )
       )
     );
   }
