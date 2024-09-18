a3485186c61f9795b69c5a7ce46b38d4a169486e
Trol3k <maciej-sikora4@hotmail.com>
2024-09-18 23:34:18 +0200

    Added displaying location history

--- atgs_app/lib/app.dart
+++ atgs_app/lib/app.dart
@@ -6,7 +6,7 @@ import 'package:atgs_app/pages/device.dart';
 const backgroundColor = Color.fromARGB(255, 29, 174, 239);
 const darkestBlue = Color.fromARGB(255, 9, 113, 160);
 const darkBlue = Color.fromARGB(255, 21, 144, 199);
-const lightBlue =Color.fromARGB(255, 151, 222, 254);
+const lightBlue = Color.fromARGB(255, 151, 222, 254);
 const lightGrey = Color.fromARGB(255, 231, 230, 230);
 const introductionDark = Color.fromARGB(255, 0, 105, 152);
 const introductionLight = Color.fromARGB(255, 179, 208, 241);
@@ -25,42 +25,37 @@ class AppView extends StatefulWidget {
   @override
   State<AppView> createState() => AppViewState();
 }
+
 class AppViewState extends State<AppView> {
   static int selectedIndex = 0;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        leading: const Badge(isLabelVisible: false, child: Icon(Icons.directions_bike, size: 60)),
+        leading: const Badge(
+            isLabelVisible: false,
+            child: Icon(Icons.directions_bike, size: 60)),
       ),
       extendBodyBehindAppBar: true,
-      body: Stack(
-        children: [
-          widgetOptions.elementAt(selectedIndex),
-          Positioned(
-            bottom: 0, 
+      body: Stack(children: [
+        widgetOptions.elementAt(selectedIndex),
+        const Positioned(
+            bottom: 0,
             left: 0,
             right: 0,
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              children: [
-                Container(
-                    height: 35,
-                    decoration: const BoxDecoration(color: lightGrey, borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
-                  )
-              ]
-            )
-          )
-        ]
-      ),
+            ))
+      ]),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
-              icon: Badge(isLabelVisible: false, child: Icon(Icons.location_pin)),
-              label: 'Map'),           
+              icon:
+                  Badge(isLabelVisible: false, child: Icon(Icons.location_pin)),
+              label: 'Map'),
           BottomNavigationBarItem(
               icon: Badge(isLabelVisible: false, child: Icon(Icons.settings)),
               label: 'Device'),
@@ -68,16 +63,24 @@ class AppViewState extends State<AppView> {
               icon: Badge(isLabelVisible: false, child: Icon(Icons.person)),
               label: 'Profile'),
         ],
-        selectedIconTheme: const IconThemeData(color: backgroundColor, size: 32),
+        selectedIconTheme:
+            const IconThemeData(color: backgroundColor, size: 32),
         unselectedIconTheme: const IconThemeData(color: Colors.grey, size: 32),
-        selectedLabelStyle: TextStyle(foreground: Paint() ..color = backgroundColor, fontWeight: FontWeight.w700, fontSize: 15),
-        unselectedLabelStyle: TextStyle(foreground: Paint() ..color = Colors.grey, fontWeight: FontWeight.w700, fontSize: 15),
-
+        selectedLabelStyle: TextStyle(
+            foreground: Paint()..color = backgroundColor,
+            fontWeight: FontWeight.w700,
+            fontSize: 15),
+        unselectedLabelStyle: TextStyle(
+            foreground: Paint()..color = Colors.grey,
+            fontWeight: FontWeight.w700,
+            fontSize: 15),
         backgroundColor: Colors.white,
         currentIndex: selectedIndex,
         elevation: 0,
         onTap: (index) => {
-          setState(() {selectedIndex = index;})
+          setState(() {
+            selectedIndex = index;
+          })
         },
       ),
     );
--- atgs_app/lib/pages/map.dart
+++ atgs_app/lib/pages/map.dart
@@ -6,6 +6,8 @@ import 'package:latlong2/latlong.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:atgs_app/message_service.dart';
 import 'package:atgs_app/app.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:geocode/geocode.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -18,23 +20,26 @@ class MapPageState extends State<MapPage> {
   LatLng mapLatLng = const LatLng(50.22718711817035, 22.91607329997953);
   double mapZoom = 17;
   double mapRotation = 0;
-  bool initial = true;
-  bool showTimestamp = false;
-  DateTime? date;
-  double timeStampBoxOpacity = 1.0;
+  bool showActualPosition = true;
+  bool showPolyline = false;
+  DateTime? timeStamp;
+  List<String> addressHistory = [];
+  List<LatLng> locationHistory = [];
 
   final MapController mapController = MapController();
+  GeoCode geoCode = GeoCode();
 
-  void showDeviceLocation() {
-    sendMessage("location");
+  Future<void> showDeviceLocation() async {
+    if (!deviceArmed) sendMessage("location");
+
     mapController.move(mapLatLng, mapZoom);
     mapController.rotate(mapRotation);
   }
 
   void updateLocation() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    
-    if(AppViewState.selectedIndex == 0) {
+
+    if (AppViewState.selectedIndex == 0) {
       await prefs.reload();
       double? latitude = prefs.getDouble("latitude");
       double? longitude = prefs.getDouble("longitude");
@@ -45,42 +50,31 @@ class MapPageState extends State<MapPage> {
       debugPrint("-UPDATE- Date: $stringDate");
 
       if (latitude != null && longitude != null && mounted) {
-        setState(() {
-          mapLatLng = LatLng(latitude, longitude);
+        Address address =
+            await geoCode.reverseGeocoding(latitude: latitude, longitude: longitude);
 
-          if(stringDate != null) {
-            DateTime parsedDate = DateFormat("yyyy-MM-dd HH:mm:ss").parse(stringDate, true).toLocal();
-            date = parsedDate;
+        setState(() {
+          if (showActualPosition) mapLatLng = LatLng(latitude, longitude);
+
+          if (stringDate != null) {
+            DateTime parsedDate = DateFormat("yyyy-MM-dd HH:mm:ss")
+                .parse(stringDate, true)
+                .toLocal();
+
+            if (parsedDate != timeStamp) {
+              timeStamp = parsedDate;
+
+              addressHistory.add(
+                  "${address.streetAddress ?? ''} ${address.streetNumber ?? ''}, ${address.postal != null ? '${address.postal!.substring(0, 2)}-${address.postal!.substring(2, 5)}' : ''} ${address.city} - ${DateFormat("dd-MM-yyyy HH:mm").format(parsedDate)}");
+              locationHistory.add(LatLng(latitude, longitude));
+            }
           }
         });
       }
-      if (initial) {
+      if (showActualPosition) {
         mapController.move(mapLatLng, mapZoom);
-        initial = false;
       }
     }
-  }
-
-  onTapMarker ({required BuildContext context}) async {
-    setState(() {
-      showTimestamp = true;
-      timeStampBoxOpacity = 1.0;
-    });
-    
-    Timer(const Duration(seconds: 1), () {
-      if(AppViewState.selectedIndex == 0){
-        setState(() {
-          timeStampBoxOpacity = 0.0;
-        });
-        Timer(const Duration(seconds: 1), () {
-          if(AppViewState.selectedIndex == 0){
-            setState(() {
-              showTimestamp = false;
-            });
-          }
-        });
-      }   
-    });
   }
 
   late Timer timer;
@@ -102,93 +96,54 @@ class MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
+      body: SlidingUpPanel(
+        minHeight: 65,
+        maxHeight:
+            MediaQuery.of(context).size.height * 0.30, // 30% of the screen
+        borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
+        panel: _buildLocationHistoryPanel(),
+        body: Stack(children: [
           FlutterMap(
-            mapController: mapController,
-            options: MapOptions(
-              center: mapLatLng,
-              zoom: mapZoom,
-              maxZoom: 18,
-              minZoom: 11,
-            ),
-            children: [
-              TileLayer(
-                urlTemplate:
-                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: const ['a', 'b', 'c'],
-                userAgentPackageName: 'com.example.app',
+              mapController: mapController,
+              options: MapOptions(
+                center: mapLatLng,
+                zoom: mapZoom,
+                maxZoom: 18,
+                minZoom: 11,
               ),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    point: mapLatLng,
-                    width: 80,
-                    height: 80,
-                    builder: (context) {
-                      return GestureDetector(
-                        onTap: () => onTapMarker(context: context),
-                        child: Stack(
-                          alignment: Alignment.center,
-                          clipBehavior: Clip.none,
-                          children: [
-                            Center(
-                              child: Stack(
-                                alignment: Alignment.center,
-                                children: [
-                                  const Icon(
-                                    Icons.place,
-                                    color: backgroundColor,
-                                    size: 60
-                                  ),
-                                  Positioned(
-                                    top: 10,
-                                    child: Container(
-                                      width: 30,
-                                      height: 30,
-                                      decoration: const BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: backgroundColor
-                                      )
-                                    )
-                                  ),
-                                  const Positioned(
-                                    top: 7,
-                                    child: Icon(
-                                      Icons.pedal_bike,
-                                      color: Colors.white,
-                                      size: 30,
-                                    )
-                                  )
-                                ]
-                              )
-                            ),
-                            if (showTimestamp) 
-                              Positioned(
-                                bottom: 70,
-                                child: AnimatedOpacity(
-                                  duration: const Duration(seconds: 1),
-                                  opacity: timeStampBoxOpacity,
-                                  child: Container(
-                                    padding: const EdgeInsets.all(10.0),
-                                    decoration: BoxDecoration(color: backgroundColor, borderRadius: BorderRadius.circular(10.0), border: Border.all(color: darkBlue, width: 2)),
-                                    child: Column(
-                                      children: [
-                                        Text("${mapLatLng.latitude}, ${mapLatLng.longitude}", style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w900)),
-                                        Text( (date != null) ? DateFormat('HH:mm   dd.MM.yyyy').format(date!) : "N/A", style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontStyle: FontStyle.italic)),
-                                    ],)   
-                                  )
-                                )
-                              ),
-                          ]
-                        )
-                      );
-                    }
-                  )
-                ]
-              )
-            ]
-          ),
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: const ['a', 'b', 'c'],
+                  userAgentPackageName: 'com.example.app',
+                ),
+                PolylineLayer(
+                  polylines: [
+                    if (showPolyline)
+                      Polyline(
+                        strokeWidth: 4,
+                        points: locationHistory,
+                        color: Colors.blue,
+                      ),
+                  ],
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: mapLatLng,
+                      width: 80,
+                      height: 80,
+                      builder: (context) {
+                        return CustomMarkerWidget(
+                          location: mapLatLng,
+                          timestamp: timeStamp,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ]),
           Align(
             alignment: Alignment.topCenter,
             child: Padding(
@@ -198,20 +153,190 @@ class MapPageState extends State<MapPage> {
                   padding: const EdgeInsets.all(20),
                   side: const BorderSide(color: backgroundColor, width: 5),
                 ),
-                onPressed: showDeviceLocation,
+                onPressed: () {
+                  setState(() {
+                    showActualPosition = true;
+                    showPolyline = false;
+                  });
+                  showDeviceLocation();
+                },
                 child: const Text(
                   "Show my bike",
                   style: TextStyle(
                     color: backgroundColor,
                     fontSize: 25,
                     fontWeight: FontWeight.w900,
-                  )
-                )
-              )
-            )
+                  ),
+                ),
+              ),
+            ),
           )
-        ]
-      )
+        ]),
+      ),
+    );
+  }
+
+  Widget _buildLocationHistoryPanel() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Location History',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      showPolyline = true;
+                      showActualPosition = false;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: backgroundColor),
+                  child: const Text(
+                    "Show on map",
+                    style: TextStyle(color: Colors.white),
+                  ))
+            ],
+          ),
+          const Divider(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: addressHistory.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading:
+                      const Icon(Icons.location_on, color: backgroundColor),
+                  title: Text(addressHistory[index],
+                      style: const TextStyle(fontSize: 16)),
+                  onTap: () {
+                    setState(() {
+                      showActualPosition = false;
+                      mapLatLng = locationHistory[index];
+                    });
+                    showDeviceLocation();
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMarkerWidget extends StatefulWidget {
+  const CustomMarkerWidget({
+    super.key,
+    required this.location,
+    required this.timestamp,
+  });
+
+  final LatLng location;
+  final DateTime? timestamp;
+
+  @override
+  State<CustomMarkerWidget> createState() => CustomMarkerWidgetState();
+}
+
+class CustomMarkerWidgetState extends State<CustomMarkerWidget> {
+  bool showTimestamp = false;
+  double timestampBoxOpacity = 0.0;
+
+  onTapMarker() async {
+    setState(() {
+      showTimestamp = true;
+      timestampBoxOpacity = 1.0;
+    });
+
+    Timer(const Duration(seconds: 1), () {
+      if (AppViewState.selectedIndex == 0) {
+        setState(() {
+          timestampBoxOpacity = 0.0;
+        });
+        Timer(const Duration(seconds: 1), () {
+          if (AppViewState.selectedIndex == 0) {
+            setState(() {
+              showTimestamp = false;
+            });
+          }
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTapMarker,
+      child: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          Center(
+            child: Stack(alignment: Alignment.center, children: [
+              const Icon(Icons.place, color: backgroundColor, size: 60),
+              Positioned(
+                  top: 10,
+                  child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle, color: backgroundColor))),
+              const Positioned(
+                top: 7,
+                child: Icon(
+                  Icons.pedal_bike,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              )
+            ]),
+          ),
+          if (showTimestamp)
+            Positioned(
+                bottom: 70,
+                child: AnimatedOpacity(
+                  duration: const Duration(seconds: 1),
+                  opacity: timestampBoxOpacity,
+                  child: Container(
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: backgroundColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(color: darkBlue, width: 2),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "${widget.location.latitude}, ${widget.location.longitude}",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        Text(
+                          widget.timestamp != null
+                              ? DateFormat('HH:mm   dd.MM.yyyy')
+                                  .format(widget.timestamp!)
+                              : "N/A",
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontStyle: FontStyle.italic),
+                        ),
+                      ],
+                    ),
+                  ),
+                ))
+        ],
+      ),
     );
   }
 }
